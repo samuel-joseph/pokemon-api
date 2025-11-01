b38a699826a779ec7dbd0b28faaff75351755b54
arcanlte <pinangayjoel@gmail.com>
2025-11-01 13:43:05 -0400

    Forgot npc line up

--- script/buildNpc.js
+++ script/buildNpc.js
@@ -4,133 +4,130 @@ import { loadData } from "../services/pokemonServices.js";
 
 // Define gym leaders with Pokémon IDs or names
 const gymLeadersTemplate = [
-  // {
-  //   region: "kanto",
-  //   gymLeaders: [
-  //     {
-  //       name: "Blue",
-  //       pokemon: [
-  //         { name: "pidgeot", level: 75 },
-  //         { name: "exeggutor", level: 77 },
-  //         { name: "alakazam", level: 76 },
-  //         { name: "nidoking", level: 78 },
-  //         { name: "arcanine", level: 78 },
-  //         { name: "rhydon", level: 78 },
-  //         { name: "blastoise", level: 80, canMega: true },
-  //       ],
-  //     },
-  //   ],
-  // },
-  // {
-  //   region: "johto",
-  //   gymLeaders: [
-  //     {
-  //       name: "Lance",
-  //       pokemon: [
-  //         { name: "charizard", level: 80 },
-  //         { name: "dragonite", level: 80 },
-  //         { name: "dragonite", level: 78 },
-  //         { name: "dragonite", level: 80 },
-  //         { name: "aerodactyl", level: 79 },
-  //         { name: "gyarados", level: 82, canMega: true },
-  //       ],
-  //     },
-  //   ],
-  // },
-  // {
-  //   region: "hoenn",
-  //   gymLeaders: [
-  //     {
-  //       name: "Steven",
-  //       pokemon: [
-  //         { name: "skarmory", level: 81 },
-  //         { name: "claydol", level: 82 },
-  //         { name: "aggron", level: 81 },
-  //         { name: "cradily", level: 83 },
-  //         { name: "armaldo", level: 83 },
-  //         { name: "metagross", level: 84, canMega: true },
-  //       ],
-  //     },
-  //   ],
-  // },
-  // {
-  //   region: "sinnoh",
-  //   gymLeaders: [
-  //     {
-  //       name: "Cynthia",
-  //       pokemon: [
-  //         { name: "spiritomb", level: 85 },
-  //         { name: "gastrodon", level: 85 },
-  //         { name: "lucario", level: 86 },
-  //         { name: "milotic", level: 86 },
-  //         { name: "roserade", level: 84 },
-  //         { name: "garchomp", level: 87, canMega: true },
-  //       ],
-  //     },
-  //   ],
-  // },
-  // {
-  //   region: "unova",
-  //   gymLeaders: [
-  //     {
-  //       name: "Iris",
-  //       pokemon: [
-  //         { name: "hydreigon", level: 86 },
-  //         { name: "druddigon", level: 86 },
-  //         { name: "lapras", level: 86 },
-  //         { name: "aggron", level: 87 },
-  //         { name: "archeops", level: 87 },
-  //         { name: "haxorus", level: 88 },
-  //       ],
-  //     },
-  //   ],
-  // },
-  // {
-  //   region: "kalos",
-  //   gymLeaders: [
-  //     {
-  //       name: "Diantha",
-  //       pokemon: [
-  //         { name: "hawlucha", level: 87 },
-  //         { name: "tyrantrum", level: 87 },
-  //         { name: "aurorus", level: 87 },
-  //         { name: "gourgeist", level: 86 },
-  //         { name: "goodra", level: 86 },
-  //         { name: "gardevoir", level: 88, canMega: true },
-  //       ],
-  //     },
-  //   ],
-  // },
-  // {
-  //   region: "alola",
-  //   gymLeaders: [
-  //     {
-  //       name: "Kukui",
-  //       pokemon: [
-  //         { name: "lycanroc", level: 87 },
-  //         { name: "braviary", level: 87 },
-  //         { name: "magnezone", level: 87 },
-  //         { name: "snorlax", level: 86 },
-  //         { name: "ninetales", level: 86 },
-  //         { name: "decidueye", level: 88 },
-  //       ],
-  //     },
-  //   ],
-  // },
-  // {
   {
     region: "kanto",
     gymLeaders: [
       {
+        name: "Blue",
+        pokemon: [
+          { name: "pidgeot", level: 75 },
+          { name: "exeggutor", level: 77 },
+          { name: "alakazam", level: 76 },
+          { name: "nidoking", level: 78 },
+          { name: "arcanine", level: 78 },
+          { name: "rhydon", level: 78 },
+          { name: "blastoise", level: 80, canMega: true },
+        ],
+      },
+    ],
+  },
+  {
+    region: "johto",
+    gymLeaders: [
+      {
+        name: "Lance",
+        pokemon: [
+          { name: "charizard", level: 80 },
+          { name: "dragonite", level: 80 },
+          { name: "dragonite", level: 78 },
+          { name: "dragonite", level: 80 },
+          { name: "aerodactyl", level: 79 },
+          { name: "gyarados", level: 82, canMega: true },
+        ],
+      },
+    ],
+  },
+  {
+    region: "hoenn",
+    gymLeaders: [
+      {
+        name: "Steven",
+        pokemon: [
+          { name: "skarmory", level: 81 },
+          { name: "claydol", level: 82 },
+          { name: "aggron", level: 81 },
+          { name: "cradily", level: 83 },
+          { name: "armaldo", level: 83 },
+          { name: "metagross", level: 84, canMega: true },
+        ],
+      },
+    ],
+  },
+  {
+    region: "sinnoh",
+    gymLeaders: [
+      {
+        name: "Cynthia",
+        pokemon: [
+          { name: "spiritomb", level: 85 },
+          { name: "gastrodon", level: 85 },
+          { name: "lucario", level: 86 },
+          { name: "milotic", level: 86 },
+          { name: "roserade", level: 84 },
+          { name: "garchomp", level: 87, canMega: true },
+        ],
+      },
+    ],
+  },
+  {
+    region: "unova",
+    gymLeaders: [
+      {
+        name: "Iris",
+        pokemon: [
+          { name: "hydreigon", level: 86 },
+          { name: "druddigon", level: 86 },
+          { name: "lapras", level: 86 },
+          { name: "aggron", level: 87 },
+          { name: "archeops", level: 87 },
+          { name: "haxorus", level: 88 },
+        ],
+      },
+    ],
+  },
+  {
+    region: "kalos",
+    gymLeaders: [
+      {
+        name: "Diantha",
+        pokemon: [
+          { name: "hawlucha", level: 87 },
+          { name: "tyrantrum", level: 87 },
+          { name: "aurorus", level: 87 },
+          { name: "gourgeist", level: 86 },
+          { name: "goodra", level: 86 },
+          { name: "gardevoir", level: 88, canMega: true },
+        ],
+      },
+    ],
+  },
+  {
+    region: "alola",
+    gymLeaders: [
+      {
+        name: "Kukui",
+        pokemon: [
+          { name: "lycanroc", level: 87 },
+          { name: "braviary", level: 87 },
+          { name: "magnezone", level: 87 },
+          { name: "snorlax", level: 86 },
+          { name: "ninetales", level: 86 },
+          { name: "decidueye", level: 88 },
+        ],
+      },
+    ],
+  },
+  {
+    region: "galar",
+    gymLeaders: [
+      {
         name: "Leon",
         pokemon: [
-          // { name: "aegislash", level: 89 },
-          // { name: "dragapult", level: 92 },
-          // { name: "haxorus", level: 90 },
-          // { name: "rhyperior", level: 91 },
-          // { name: "seismitoad", level: 88 },
-          { name: "charizard", level: 95, canMega: true },
-          { name: "charizard", level: 95, canMega: true },
+          { name: "aegislash", level: 89 },
+          { name: "dragapult", level: 92 },
+          { name: "haxorus", level: 90 },
+          { name: "rhyperior", level: 91 },
+          { name: "seismitoad", level: 88 },
           { name: "charizard", level: 95, canMega: true },
         ],
       },
